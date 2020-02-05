134947c4d30be3ca566675e3e5a80e44cca985c2
Evgeny Orekhov <eorekhov@axcient.com>
2020-02-05 14:04:59 +0300

    "No files matching the pattern" bug

--- test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-/* eslint-disable import/no-unassigned-import */
-
-"use strict";
-
-require("./index.json");
-require("./fp.json");
-require("./node.json");
-require("./disabled-rules.json");
