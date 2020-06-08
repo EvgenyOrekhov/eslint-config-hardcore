0df674ea34f3bb5bd6de3c696bc659665a2251f0
Evgeny Orekhov <eorekhov@axcient.com>
2020-06-08 20:12:21 +0300

    Fix "ESLint couldn't find the config "./base.json" to extend from"

--- test.js
+++ test.js
@@ -3,7 +3,9 @@
 
 "use strict";
 
+require("./base.json");
 require("./index.json");
 require("./fp.json");
 require("./node.json");
 require("./disabled-rules.json");
+require("./rules-disabled-by-prettier.json");
