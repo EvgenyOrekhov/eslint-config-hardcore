635d096e0de5ecac90ca3887f292f7031d2f15c4
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2021-07-15 13:13:56 +0300

    Update dependencies and add failing test case
    Ref https://github.com/coderaiser/putout/issues/67

--- esm/test.js
+++ esm/test.js
@@ -1,2 +1,8 @@
+function foo({ bar }) {
+  return { bar };
+}
+
+foo({ bar: 123 });
+
 // eslint-disable-next-line import/no-unused-modules, import/no-anonymous-default-export
 export default "test";
