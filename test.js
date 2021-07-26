6a3dd989533cd983992519e08d7e8abe13e89559
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2021-07-26 20:35:13 +0300

    Add `hardcore/react`, update `hardcore/ts`

--- test.js
+++ test.js
@@ -14,6 +14,7 @@ require("./fp.json");
 require("./node.json");
 require("./ts-for-js.json");
 require("./ts.json");
+require("./react.json");
 require("./tsconfig.json");
 require("./disabled-rules.json");
 require("./rules-disabled-by-prettier.json");
