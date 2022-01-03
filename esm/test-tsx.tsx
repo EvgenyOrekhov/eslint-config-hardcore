378d4ac0b9630c93b348172df32303561303de98
Evgeny Orekhov <EvgenyOrekhov@gmail.com>
2022-01-03 19:08:11 +0300

    Enforce 80 column limit for `eslint-disable-next-line` comments
    Use multiline comment if needed:
    
    ```
    /*
      eslint-disable-next-line
      ruleA,
      ruleB
      -- some long explanation
    */
    ```

--- esm/test-tsx.tsx
+++ esm/test-tsx.tsx
@@ -1,2 +1,6 @@
-// eslint-disable-next-line import/no-unused-modules, import/no-anonymous-default-export
+/*
+  eslint-disable-next-line
+  import/no-unused-modules,
+  import/no-anonymous-default-export
+*/
 export default "test";
